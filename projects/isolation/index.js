43ca4a4aa30fff9bcf8a1120d639300f749b98b9
ethanhalpern <halpm171@newschool.edu>
2020-04-16 13:07:43 -0400

    new

--- projects/isolation/index.js
+++ projects/isolation/index.js
@@ -1,8 +1,29 @@
-$('.open-lightbox').click(function() {
-  $('lightbox').addClass('lightbox-is-open');
-});
+$(function() {
 
-$('.close-lightbox').click(function() {
-  $('lightbox').removeClass('lightbox-is-open');
-});
+  // Start of your code:
 
+
+
+  $('.open-lightbox').click(function() {
+
+    $('body').addClass('lightbox-is-open');
+
+  });
+
+
+
+  $('.close-lightbox').click(function() {
+
+    $('body').removeClass('lightbox-is-open');
+
+  });
+
+
+
+  // End of your code
+
+
+
+// end of special jquery function
+
+});
