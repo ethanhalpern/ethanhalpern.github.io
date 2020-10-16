1b8588eda0b42e49451e9fa4d4d05c72465e700c
ethanhalpern <halpm171@newschool.edu>
2020-10-15 20:29:20 -0400

    new

---
+++ projects/one/index.js
@@ -0,0 +1,10 @@
+var source = "assets/first.m4a"
+ var audio = document.createElement("audio");
+ //
+ audio.autoplay = true;
+ //
+ audio.load()
+ audio.addEventListener("load", function() { 
+     audio.play(); 
+ }, true);
+ audio.src = source;
