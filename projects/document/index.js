5dd6210ed8e872cea0b17c42e24f33e96effdf4b
ethanhalpern <halpm171@newschool.edu>
2020-10-22 23:26:06 -0400

    new

---
+++ projects/document/index.js
@@ -0,0 +1,14 @@
+function myFunction() {
+  var x, text;
+
+  // Get the value of the input field with id="numb"
+  x = document.getElementById("numb").value;
+
+  // If x is Not a Number or less than one or greater than 10
+  if (x==4130) {
+    text = "Correct please go to this website";
+  } else {
+    text = "incorrect. please try again!";
+  }
+  document.getElementById("demo").innerHTML = text;
+}
